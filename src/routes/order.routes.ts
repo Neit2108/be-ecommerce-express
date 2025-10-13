d878c418b0a24fdfef2f8a5567da0aea679744f5
Neit2108 <tienld.22810310329@epu.edu.vn>
2025-10-13 15:08:57 +0700

    12/10/2025 : - add route for order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,15 +1,47 @@
-import { Router } from "express";
-import { combineMiddleware } from "../utils/middleware.util";
-import { authenticateToken, requireStatus } from "../middleware/auth.middleware";
-import { UserStatus } from "@prisma/client";
-import { orderController } from "../controllers/order.controller";
+import { Router } from 'express';
+import { combineMiddleware } from '../utils/middleware.util';
+import {
+  authenticateToken,
+  requireRole,
+  requireStatus,
+} from '../middleware/auth.middleware';
+import { RoleType, UserStatus } from '@prisma/client';
+import { orderController } from '../controllers/order.controller';
 
 const router = Router();
 
-router.post('/', combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])), orderController.createOrder);
-router.get('/:orderId', combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])), orderController.getOrderById);
-router.get('/', combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])), orderController.getMyOrders);
-router.put('/:orderId/status', combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])), orderController.updateOrderStatus);
-router.post('/:orderId/cancel', combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])), orderController.cancelOrder);
-
+router.post(
+  '/',
+  combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])),
+  orderController.createOrder
+);
+router.get(
+  '/:orderId',
+  combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])),
+  orderController.getOrderById
+);
+router.get(
+  '/',
+  combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])),
+  orderController.getMyOrders
+);
+router.put(
+  '/:orderId/status',
+  combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])),
+  orderController.updateOrderStatus
+);
+router.post(
+  '/:orderId/cancel',
+  combineMiddleware(authenticateToken, requireStatus([UserStatus.ACTIVE])),
+  orderController.cancelOrder
+);
+router.post(
+  '/:orderId/confirm',
+  combineMiddleware(
+    authenticateToken,
+    requireStatus([UserStatus.ACTIVE]),
+    requireRole(RoleType.SELLER, RoleType.SYSTEM_ADMIN)
+  ),
+  orderController.confirmOrder
+);
 export default router;
